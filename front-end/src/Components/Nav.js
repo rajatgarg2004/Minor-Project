99d59abdfb839bbb442213d1f7a6fcb9177e14e3
rajatgarg2004 <rajatgarg.bt21ele@pec.edu.in>
2023-12-18 16:37:01 +0530

    minor-changes

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -8,9 +8,9 @@ const Nav= ()=>{
         <div>
             <h1 style={{"font-size":"50px","color":"#D9FB00"}}>Welcome to Electrical Dashboard</h1>
             <ul className='nav-ul' style={{"backgroundColor":"#D9FB00"}}>
-                <li style={{"color":"black"}}><Link to="/UCDP">Unit Commitment</Link></li>
-                <li><Link to="/YWG">Year-Wise Generation</Link></li>
-                <li><Link to="/SWG">State-Wise Requirement</Link></li>
+                <li><Link to="/UCDP"><span style={{"color":"black","fontWeight":"bolder","fontSize":"20px"}}>Unit Commitment</span></Link></li>
+                <li><Link to="/YWG"><span style= {{"color":"black","fontWeight":"bolder","fontSize":"20px"}}>Year-Wise Generation</span></Link></li>
+                <li><Link to="/SWG"><span style= {{"color":"black","fontWeight":"bolder","fontSize":"20px"}}>State-Wise Requirement</span></Link></li>
 
             </ul>
         </div>
